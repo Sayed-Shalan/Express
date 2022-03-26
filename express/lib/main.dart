34d34368870f633ca04e73213f3d528f5c9035fd
Sayed-Shalan <sayedshalan22@gmail.com>
2022-03-26 19:01:50 +0200

    fix issues

--- express/lib/main.dart
+++ express/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Express',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFFbd751b),
         primaryColorDark: const Color(0xFFbd751b)
